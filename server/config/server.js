5713bb941bf91fb7bbf8153c71f7e35020fa6dcd
Arthur Brito Medeiros <arthurmedeiros32@gmail.com>
2021-11-23 15:56:18 -0300

    first commit

---
+++ server/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '5e48ab3d9366a854cc42ac980477d9e7'),
+    },
+  },
+});
---
+++ server/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'b8e4acc0-f778-4bdb-99f8-59cc916ff0a3'
+};
