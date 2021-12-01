0ebdb0d6bfef166b59706d8e888a2e141d85fbfa
M4Falcao <mateus.souza.falcao@gmail.com>
2021-12-01 00:54:29 -0300

    feature/footer: Added all main elementes

--- client/src/assets/index.js
+++ client/src/assets/index.js
@@ -1,5 +1,17 @@
-import Logo from './logo.png';
+import Logo from "./logo.png";
+import miniLogo from "./miniLogo.svg";
+import logoCeleiro from "./logo-celeiro.png";
+import fundo from "./fundo.png";
+import instagramButton from "./instagramButton.svg";
+import linkedinButton from "./linkedinButton.svg";
+import facebookButton from "./facebookButton.svg";
 
 export {
   Logo,
+  miniLogo,
+  fundo,
+  logoCeleiro,
+  instagramButton,
+  linkedinButton,
+  facebookButton,
 };
--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,11 +1,13 @@
 import React from "react"; //obrigatorio
 import styles from './Footer.module.css';  //importa a folha de estilos que tem que estar na mesma pasta
-import { Logo } from '../../assets'
+import { miniLogo, fundo, logoCeleiro , instagramButton, facebookButton, linkedinButton } from '../../assets/index'
 
-const Footer = () => {
-  return (
-    <div className={styles.minha}>
-      <p>
+function citiText() {
+
+
+  return(
+    <div className={styles.citiText}>
+      <p font-size='16px' >
         Made with
         {' '}
         <strong>&lt; &#x0002F; &gt;</strong>
@@ -14,10 +16,38 @@ const Footer = () => {
         {' '}
         <strong>&hearts;</strong>
         {' '}
-        by <img src={Logo} alt="" />
+        by <img width='29px' src={miniLogo} alt="" />
       </p>
+    </div>
+  );
+};
+
+const Footer = () => {
+  const adress = 'rua dos bobos';
+  const phone = '70707070';
+
+  return (
+    <div className={styles.main}>
+      <div className={styles.container}>
+        <div className={styles.information}>
+          <img src={logoCeleiro} alt="logo do celeiro de bambas" />
+          <h3>ASSOCIAÇÃO CELEIRO DE BAMBAS</h3>
+          <p>{adress}</p>
+          <p>{phone}</p>
+        </div>
+
+        <div className={styles.socialMedia}>
+          <p font-size='16px' >Nos siga nas redes sociais</p>
+          <a target="_blank" href="instagram.com"><img src={instagramButton} alt="instagram" /></a>
+          <a target="_blank" href="linkedin.com"><img src={linkedinButton} alt="linkedin" /></a>
+          <a target="_blank" href="facebook.com"><img src={facebookButton} alt="facebook" /></a>
+        </div>
+      </div>
+
+      {citiText()}
+
+      {/* <img className={styles.fundo} src={fundo} alt="" /> */}
     </div>
   )
 }
-
-export default Footer;
+  export default Footer;
