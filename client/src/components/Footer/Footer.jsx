fa1fa672b77fcea79e1780071e9bccfb6cd2fb6c
M4Falcao <mateus.souza.falcao@gmail.com>
2021-12-02 19:35:51 -0300

    feature(footer): finished the axios

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,13 +1,14 @@
 import React, { useState, useEffect } from "react"; //obrigatorio
 import styles from './Footer.module.css';  //importa a folha de estilos que tem que estar na mesma pasta
-import { miniLogo, logoCeleiro , instagramButton, facebookButton, youtubeButton } from '../../assets/index'
-//import apiAxios from "../../services/api-axios"
+import { miniLogo, logoCeleiro , instagramButton, facebookButton, youtubeButton } from '../../assets/index';
+import apiAxios from "../../services/api-axios";
+
 
 
 function citiText() {
   return(
     <div className={styles.citiText}>
-      <p font-size='16px' >
+      <p >
         Made with
         {' '}
         <strong>&lt; &#x0002F; &gt;</strong>
@@ -16,7 +17,9 @@ function citiText() {
         {' '}
         <strong>&hearts;</strong>
         {' '}
-        by <img width='29px' src={miniLogo} alt="" />
+        by
+        {' '}
+        <img src={miniLogo} alt="logo-citi" />
       </p>
     </div>
   );
@@ -24,29 +27,32 @@ function citiText() {
 
 const Footer = () => {
 
-//   const [adress, setAdress] = useState();
-// const [phone, setPhone] = useState();
-// const [instagram, setInstagram] = useState();
-// const [youtube, setYoutube] = useState();
-// const [facebook, setFacebook] = useState();
+const [adress, setAdress] = useState();
+const [phone, setPhone] = useState();
+const [instagram, setInstagram] = useState();
+const [youtube, setYoutube] = useState();
+const [facebook, setFacebook] = useState();
 
+function changePhone(phone) {
+  const prt1 = phone.slice(0,2);
+  const prt2 = phone.slice(2,7);
+  const prt3 = phone.slice(7,11);
+  return ('('+prt1+') '+prt2+'-'+prt3)
+}
 
-// //  const loadData = async () => {
-// //    const data = await apiAxios.get('Footer');
-// //    setAdress(data.Endereco);
-// //  };
+ const loadData = async () => {
+   const {data} = await apiAxios.get('footer');
+   console.log(data);
+   setAdress(data.Endereco);
+   setPhone(changePhone(data.Telefone));
+   setInstagram(data.Instagram);
+   setYoutube(data.YouTube);
+   setFacebook(data.Facebook);
+ };
 
-//  useEffect(()=>{
-//   setAdress('lugar');
-//    //loadData();
-//  },[]);
-
-  const adress = 'rua dos bobos, n 0'
-  const phone = '70707070';
-  const instagram = 'instagram.com';
-  const youtube = 'youtube.com';
-  const facebook = 'fabebook.com';
-
+ useEffect(()=>{
+   loadData();
+ });
 
   return (
     <div className={styles.main}>
@@ -54,13 +60,15 @@ const Footer = () => {
 
         <div className={styles.information}>
           <img src={logoCeleiro} alt="logo do celeiro de bambas" />
-          <h3 className='name'>ASSOCIAÇÃO CELEIRO DE BAMBAS</h3>
-          <p className='adress' >{adress}</p>
-          <p className='phone'>{phone}</p>
+          <div>
+            <h3 className='name'>ASSOCIAÇÃO CELEIRO DE BAMBAS</h3>
+            <p className='adress' >{adress}</p>
+            <p className='phone'>{phone}</p>
+          </div>
         </div>
 
         <div className={styles.socialMedia}>
-          <p font-size='16px' >Nos siga nas redes sociais</p>
+          <p className={styles.text} font-size='16px' >Nos siga nas redes sociais</p>
           <div className={styles.medias}>
             <a target="_blank" rel="noreferrer" href={instagram}><img src={instagramButton} alt="instagram" /></a>
             <a target="_blank" rel="noreferrer" href={youtube}><img src={youtubeButton} alt="linkedin" /></a>
@@ -70,9 +78,11 @@ const Footer = () => {
       </div>
 
       <div className={styles.citiText}>
+        <div></div>
         {citiText()}
       </div>
     </div>
   )
 }
-  export default Footer;
+
+export default Footer;
