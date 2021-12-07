3311953c5a1062dfb24902dfa92b168a4a2e912c
Luiz Fernando da Silva Santos <luiz.silvasantos@ufpe.br>
2021-12-07 15:48:31 -0300

    Hotfix(feature/banner-section): I've started axios integration

--- client/src/components/Banner/Banner.jsx
+++ client/src/components/Banner/Banner.jsx
@@ -7,15 +7,14 @@ function Banner() {
 
   return (
       <div className={styles.container}>
-          <img className={styles.backImageMask} src={mask} alt = "mascara" />
-          <img className={styles.backImageBanner} src={bannerimage} alt="Foto das crianças" />
+          <img className={styles.backImageMask} src={mask} alt = "mascara da foto" />
+          <img className={styles.backImageBanner} src={bannerimage} alt="Foto das crianças e voluntários do Celeiro de Bambas" />
           <spam className = {styles.text}>
             <p className={styles.title}>Celeiro de Bambas</p>
             <p className={styles.subTitle}>Desenvolvendo as potencialidades do ser humano.</p>
           </spam>
       </div>
   );
-
 }
 
 
