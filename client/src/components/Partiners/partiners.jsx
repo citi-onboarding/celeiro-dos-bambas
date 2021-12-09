3706a3883d37467b0027033b36c4e359d082f81a
M4Falcao <mateus.souza.falcao@gmail.com>
2021-12-08 21:27:34 -0300

    (feature/partiners): I made the slike works

--- client/src/components/Partiners/partiners.jsx
+++ client/src/components/Partiners/partiners.jsx
@@ -1,45 +1,96 @@
-import React, { useState, useEffect } from "react";
-import styles from "./partiners.module.css";
+// Basic Imports
+import React, { useEffect, useState } from "react";
+import './slick.css';
 import apiAxios from "../../services/api-axios";
+import styles from "./partiners.module.css"
+// import BoxMember from "./boxMember";
+
+// Carousel Dependences
+import Slider from 'react-slick';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 
-const Partiners = () => {
+function Partiners(){
 
-  const [name, setName] = useState([]);
-  const [description, setDescription] = useState([]);
-  const [logo, setLogo] = useState([]);
+  // Arrows
+  function NextArrowCenter(props) {
+    const { className, style, onClick } = props;
+    return (
+      <div
+        className={className}
+        style={{ ...style, display: "block", background: "grey" }}
+        onClick={onClick}
+      />
+    );
+  }
+
+  function PrevArrowCenter(props) {
+    const { className, style, onClick } = props;
+    return (
+      <div
+        className={className}
+        style={{ ...style, display: "block", background: "grey" }}
+        onClick={onClick}
+      />
+    );
+  }
+
+  // Load Members Data
+  const [partiners, setPartiners] = useState([]);
 
   const loadData = async () => {
-    const {data} = await apiAxios.get('parceiros');
-    console.log(data)
-    setName(data[0].Nome);
-    setDescription(data[0].Papel);
-    setLogo(data[0].Imagem.url);
+    const res = await apiAxios.get('parceiros');
+    setPartiners(res.data);
+
   };
 
   useEffect(()=>{
     loadData();
-  });
+  }, []);
 
-  return (
-    <div className={styles.container}>
+  // Montar carrocel
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    nextArrow: <NextArrowCenter />,
+    prevArrow: <PrevArrowCenter />,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    pauseOnHover: true
+  };
+  // Estilizar
+
+  // Front
+  return(
+    <div id="partiners" className={styles.container}>
       <p>Apoiadores</p>
-
       <div className={styles.carousel}>
 
         <div className={styles.text}>
-          <p className={styles.name}>{name}</p>
-          <p className={styles.description}>{description}</p>
+          <p className={styles.name}></p>
+          <p className={styles.description}></p>
         </div>
 
-        <div>
-          <img className={styles.logo} src={logo} alt="logo do parceiro" />
+        <div className="carousel">
+          <Slider className="slider" {...settings}>
+            {partiners.map( (partiner) => {
+              return (
+                <img className="logo" src={partiner.Imagem.url} alt={partiner.Nome} />
+                );
+              })}
+          </Slider>
         </div>
 
       </div>
 
+
     </div>
-  )
+  );
 };
+
 
 export default Partiners;
