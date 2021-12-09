c0d90a1151d05406be0eeff5de3b2590c749dfc5
matheusdalia <matheusdalia8@gmail.com>
2021-12-09 11:41:25 -0300

    Hotfix(Member-section): Photo alignment, font adjustment and border radius

--- client/src/components/Members/boxMember.jsx
+++ client/src/components/Members/boxMember.jsx
@@ -6,8 +6,8 @@ export default class BoxMember extends React.Component{
   render(){
     return(
       <div className={styles.card}>
-        <p className={styles.cardName}>{this.props.nome}</p>
         <img className={styles.cardAvatar} src={this.props.avatar} alt="" />
+        <p className={styles.cardName}>{this.props.nome}</p>
         <p className={styles.cardRole}>{this.props.cargo}</p>
       </div>
     )
