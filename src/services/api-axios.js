068a9c45ed9f50fa28b408d331c1754851fd3689
M4Falcao <mateus.souza.falcao@gmail.com>
2021-12-09 23:14:14 -0300

    Update api-axios.js

--- src/services/api-axios.js
+++ src/services/api-axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiAxios = axios.create({
-  baseURL: "https://celeiro-bambas-server.herokuapp.com/",
+  baseURL: "https://http://localhost:1337/",
 });
 
 export default apiAxios;
