b8d9c3a0f329b32ded9e6862d11b1163b24343a0
M4Falcao <mateus.souza.falcao@gmail.com>
2021-12-09 22:58:38 -0300

    (ultrafiz/feature): atulized heroku

--- src/services/api-axios.js
+++ src/services/api-axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiAxios = axios.create({
-  baseURL: "https://celeiro-bambas-server.herokuapp.com/",
+  baseURL: "http://localhost:1337/",
 });
 
 export default apiAxios;
